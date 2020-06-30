382be321182fa6e807939fb86600652328681719
TheSingingFox <singingfox123@gmail.com>
2020-06-30 11:18:31 +0200

    changed program to read external data file

--- main.cpp
+++ main.cpp
@@ -1,44 +1,48 @@
 #include<iostream>
 #include<fstream>
-#include"seats.h"
 using namespace std;
 
 int main(){
 
 	int num;
 	int rows;
+	int k = 4;
+	string line;
+	ifstream seats;
+	seats.open ("seats");
 	
 	cout << "How long are the rows?" << endl;
 	cin >> rows;
 	cout << "Who do you want? (number)" << endl;
 	cin >> num;
 
-	string *seats;
-
-	seats = list();
-
 	num = num - rows;
-
-	int len = *(&seats + 1) - seats;
 
 	for(int i = 0; i < 3; i++){
 		if(num < 0){
 			num = num + rows;
-		} else if(num > len){
-			i++;
 		} else{
-			if((num + rows) % rows != 1){
-				cout << seats[num - 2][1];
+			for(int j = 1; j < num; j++){
+				getline(seats, line, '\n');
 			}
-			cout << " " << seats[num - 1][1] << " ";
+			if((num + rows) % rows != 1){
+				cout << line; 
+			}
+			getline(seats, line, '\n');
+			cout << " " << line << " ";
+			getline(seats, line, '\n');
 			if((num + rows) % rows != 0){
-				cout << seats[num][1] << endl;
+				cout << line << endl;
 			} else{
 				cout << endl;
 			}
-			num = num + rows;
+			num = num + rows - k ;
+			k++;
 		}
 	}
+
+
+	seats.close();
 
 	return 0;
 }
